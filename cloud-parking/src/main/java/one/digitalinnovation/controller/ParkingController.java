4d680fbaafc997e8c4440523a472761b471b4c8e
mikepredador <michael-ale@hotmail.com>
2022-11-20 00:34:49 -0300

    fifth commit

--- cloud-parking/src/main/java/one/digitalinnovation/controller/ParkingController.java
+++ cloud-parking/src/main/java/one/digitalinnovation/controller/ParkingController.java
@@ -3,10 +3,18 @@ package one.digitalinnovation.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import one.digitalinnovation.controller.dto.ParkingCreateDTO;
 import one.digitalinnovation.controller.dto.ParkingDTO;
 import one.digitalinnovation.controller.mapper.ParkingMapper;
 import one.digitalinnovation.model.Parking;
@@ -14,6 +22,7 @@ import one.digitalinnovation.service.ParkingService;
 
 @RestController
 @RequestMapping("/parking")
+@Api(tags = "ParkingController")
 public class ParkingController {
 	
 	private final ParkingService parkingService;
@@ -25,9 +34,23 @@ public class ParkingController {
 	}
 
 	@GetMapping
-	public List<ParkingDTO> findAll() {
+	@ApiOperation("Find all parkings")
+	public ResponseEntity<List<ParkingDTO>> findAll() {
 		List<Parking> parkingList = parkingService.findAll();
 		List<ParkingDTO> result = parkingMapper.toParkingDTOList(parkingList);
-		return result;
+		return ResponseEntity.ok(result);
+	}
+	@GetMapping("/{id}")
+	public ResponseEntity<ParkingDTO> findByID(@PathVariable String id) {
+		Parking parking = parkingService.findById(id);
+		ParkingDTO result = parkingMapper.toParkingDTO(parking);
+		return ResponseEntity.ok(result);
+	}
+	@PostMapping
+	public ResponseEntity<ParkingDTO> create(@RequestBody ParkingCreateDTO dto) {
+		var parkingCreate = parkingMapper.toParkingCreate(dto);
+		var parking = parkingService.create(parkingCreate);
+		var result = parkingMapper.toParkingDTO(parking);
+		return ResponseEntity.status(HttpStatus.CREATED).body(result);
 	}
 }
--- cloud-parking/src/main/java/one/digitalinnovation/controller/dto/ParkingDTO.java
+++ cloud-parking/src/main/java/one/digitalinnovation/controller/dto/ParkingDTO.java
@@ -2,6 +2,7 @@ package one.digitalinnovation.controller.dto;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -12,7 +13,9 @@ public class ParkingDTO {
 	private String state;
 	private String model;
 	private String color;
+	@JsonFormat(pattern = "dd/mm/yyy HH:mm")
 	private LocalDateTime entryDate;
+	@JsonFormat(pattern = "dd/mm/yyy HH:mm")
 	private LocalDateTime exitDate;
 	private Double bill;
 	
